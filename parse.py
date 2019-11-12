fb51539bb208d579e19b5144306ca304b3938f56
Peter Teitelbaum <petert926@gmail.com>
2019-11-11 18:42:32 -0800

    added datetime support

--- parse.py
+++ parse.py
@@ -1,22 +1,25 @@
 import sys, os, signal
-import re
 import time, datetime
+import re
 import requests
+import getopt
 
 # Defaults
-sampleLogUrl         = 'https://raw.githubusercontent.com/elastic/examples/master/Common%20Data%20Formats/nginx_logs/nginx_logs'
 responseCodePosition = 5
 datePosition         = 3
 
 # Initialize
 logFile         = ''
-successCount    = 0
+startTime       = ''
+endTime         = ''
+eventCount      = 0
 responseCounts  = [0 for i in range(6)]
 responsePercent = [0 for i in range(6)]
 oldestEvent     = 0
 newestEvent     = 0
-useExample      = False
+isLocalFile     = False
 
+# Define signal handler
 def handler(signum = None, frame = None):
     print('Exiting')
     exit(1)
@@ -25,60 +28,79 @@ def handler(signum = None, frame = None):
 for sig in [signal.SIGTERM, signal.SIGINT, signal.SIGHUP, signal.SIGQUIT]:
     signal.signal(sig, handler)
 
-# Use log file passed in as an argument
-try:
-  logFile = sys.argv[1]
-except:
-  noop=0 # No Op  
+# Get input parameters
+options, remainder = getopt.getopt(sys.argv[1:], 's:e:l:', ['start-time=', 'end-time=','log='])
+for opt, arg in options:
+  if opt in ('-s', '--start-time'):
+    startTime = time.mktime(datetime.datetime.strptime(arg,"%d/%b/%Y:%H:%M:%S %z").timetuple())
+  elif opt in ('-e', '--end-time'):
+    endTime = time.mktime(datetime.datetime.strptime(arg,"%d/%b/%Y:%H:%M:%S %z").timetuple())
+  elif opt in ('-l', '--log'):
+    logFile = arg
 
-# Download and use example if no log provided
+# Validate
 if '' == logFile:
-  print('No logfile provided. Using example')
-  useExample = True
+  print('Log file missing')
+  exit(1)
+if '' == startTime:
+  print('Start date/time missing')
+  exit(2)
+if '' == endTime:
+  print('End date/time missing')
+  exit(3)
+
+# Use a URL instead of a local logfile
+if 'http' == logFile[:4]:
+  isLocalFile = True
+  url=logFile
+  logFile = 'log.txt'
   try:
-    logFile = 'log.txt'
     with open(logFile, 'w+') as f:
-      r = requests.get(sampleLogUrl, data=f)
+      r = requests.get(url, data=f)
       f.write(r.text)
   except:
-    print('Failed to get example log. Exiting.')
-    exit(2)
+    print('Failed to get log. Exiting.')
+    exit(4)
 
 # Open log file
 try:
   logObj = open(logFile, 'r')
 except:
   print('File '+logFile+' does not exist')
-  exit(3)
+  exit(5)
 
-# Count rows in the log file
+# Iterate through the log file
 for logRowCount, logRow in enumerate(logObj):
   # Convert to list
   i = list(map(''.join, re.findall(r'\"(.*?)\"|\[(.*?)\]|(\S+)', logRow)))
 
-  # Iterate the response count
-  responseCounts[int(i[responseCodePosition][:1])] += 1
-
   # Get timestamp for this event
   eventTimeStamp = time.mktime(datetime.datetime.strptime(i[datePosition],"%d/%b/%Y:%H:%M:%S %z").timetuple())
 
-  # Determine the range of event times
+  # Skip this event if it is not within range
+  if not startTime <= eventTimeStamp <= endTime:
+    continue
+
+  # Increment the event count
+  eventCount += 1
+
+  # Increment the response count
+  responseCounts[int(i[responseCodePosition][:1])] += 1
+
+  # Determine the real range of event times
   if newestEvent < eventTimeStamp:
     newestEvent = eventTimeStamp
-  if oldestEvent > eventTimeStamp or logRowCount==0:
+  if oldestEvent > eventTimeStamp or 1==eventCount:
     oldestEvent = eventTimeStamp
-
-# Iterate the row count since it started at 0
-logRowCount += 1
 
 # Calculate response percentages
 for x in range(1,len(responseCounts)):
-  responsePercent[x] = round(responseCounts[x] / logRowCount * 100)
+  responsePercent[x] = round(responseCounts[x] / eventCount * 100)
 
 # Print output
-print('The site has returned a total of '+str(responseCounts[2])+' 2xx responses, '+str(responseCounts[3])+' 3xx responses, '+str(responseCounts[4])+' 4xx responses, and '+str(responseCounts[5])+' 5xx responses, out of total '+str(logRowCount)+' requests between '+time.ctime(oldestEvent)+' and '+time.ctime(newestEvent))
+print('The site has returned a total of '+str(responseCounts[2])+' 2xx responses, '+str(responseCounts[3])+' 3xx responses, '+str(responseCounts[4])+' 4xx responses, and '+str(responseCounts[5])+' 5xx responses, out of total '+str(eventCount)+' requests between '+time.ctime(oldestEvent)+' and '+time.ctime(newestEvent))
 print('That is '+str(responsePercent[4])+'% 4xx errors, '+str(responsePercent[5])+'% 5xx errors, '+str(responsePercent[3])+'% 3xx redirects, and '+str(responsePercent[2])+'% 2xx responses.')
 
 # Clean up
-if useExample:
+if isLocalFile:
   os.remove(logFile)
